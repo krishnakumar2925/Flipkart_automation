4473a281a030fb9039dcef93b26692d1f68b40cf
krishna <krishnakumar2925@gmail.com>
2025-02-12 15:32:59 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -69,7 +69,7 @@ public class TestCases {
         wrap.searchBox("Coffee Mug");
         //filter
         wrap.filterSelection("4");
-        //get the title and image url of the top 5 highest review products
+        //cdet the title and image url of the top 5 highest review products
         wrap.topReviewCount();
 
 
